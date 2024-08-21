7335666b8e0669cf909eeae27cfe97d50387395d
canerkaseler <canerkaseler@outlook.com>
2024-08-20 23:13:50 -0400

    Added @PreviewParameter for HomeScreen
    Added @PreviewParameter approach for HomeScreen.

--- app/src/main/java/com/canerkaseler/slotapipreview/HomeScreen.kt
+++ app/src/main/java/com/canerkaseler/slotapipreview/HomeScreen.kt
@@ -1,16 +1,20 @@
 package com.canerkaseler.slotapipreview
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 
 @Composable
 fun HomeScreen(
-    errorType: ErrorType = ErrorType.CONNECTION,
     modifier: Modifier = Modifier,
+    errorType: ErrorType = ErrorType.CONNECTION,
     generalMessageData: MessageCardSlot.Parameters,
     batteryMessageData: MessageCardSlot.Parameters,
     connectionMessageData: MessageCardSlot.Parameters,
@@ -39,39 +43,79 @@ fun HomeScreen(
     }
 }
 
+sealed interface HomeScreenSlot {
+    data class Parameters(
+        val errorType: ErrorType,
+        val generalMessageData: MessageCardSlot.Parameters,
+        val batteryMessageData: MessageCardSlot.Parameters,
+        val connectionMessageData: MessageCardSlot.Parameters,
+    )
+}
+
+private val fakeHomeScreenParameters =
+    HomeScreenSlot.Parameters(
+        errorType = ErrorType.CONNECTION,
+        generalMessageData = fakeGeneralErrorParameters,
+        batteryMessageData = fakeBatteryErrorMessageParameters,
+        connectionMessageData = fakeConnectionErrorMessageParameters,
+    )
+
+
+private fun interface HomeScreenPreviewData {
+    @Composable
+    fun value(): HomeScreenSlot.Parameters
+}
+
+private fun fakeHomeScreenState(
+    parameters: HomeScreenSlot.Parameters
+) = HomeScreenPreviewData(
+    function = {
+        parameters
+    }
+)
+
+private val fakeErrorMessageStateList = listOf (
+    fakeHomeScreenState(parameters = fakeHomeScreenParameters), // Default is ErrorType.CONNECTION.
+    fakeHomeScreenState(parameters = fakeHomeScreenParameters.copy(errorType = ErrorType.GENERAL)),
+    fakeHomeScreenState(parameters = fakeHomeScreenParameters.copy(errorType = ErrorType.BATTERY)),
+    fakeHomeScreenState(parameters = fakeHomeScreenParameters.copy(
+        connectionMessageData = fakeHomeScreenParameters.connectionMessageData.copy(
+            icon = R.drawable.money,
+            title = "Low balance for Internet"
+        )
+    )),
+    fakeHomeScreenState(parameters = fakeHomeScreenParameters.copy(
+        errorType = ErrorType.BATTERY,
+        batteryMessageData = fakeHomeScreenParameters.batteryMessageData.copy(
+            icon = R.drawable.fire,
+            title = "Need a new battery"
+        )
+    )),
+    fakeHomeScreenState(parameters = fakeHomeScreenParameters.copy(
+        errorType = ErrorType.GENERAL,
+        generalMessageData = fakeHomeScreenParameters.generalMessageData.copy(
+            icon = R.drawable.bug,
+            title = "If you see something, say something"
+        )
+    )),
+)
+
+private class HomeScreenPreviewProvider : PreviewParameterProvider<HomeScreenPreviewData> {
+    override val values = fakeErrorMessageStateList.asSequence()
+}
+
 @Preview(showBackground = true)
 @Composable
-private fun Preview(){
+private fun Preview(
+    @PreviewParameter(HomeScreenPreviewProvider::class) homeScreenPreviewData: HomeScreenPreviewData,
+){
     HomeScreen(
-        errorType = ErrorType.BATTERY,
         modifier = Modifier
+            .background(color = Color.Black)
             .fillMaxSize(),
-        generalMessageData = MessageCardSlot.Parameters(
-            icon = R.drawable.general_error,
-            title = "Application Problem",
-            description = "You can inform to us about it.",
-            positiveButtonText = "Send an e-mail",
-            negativeButtonText = "Close",
-            onClickPositiveButton = {},
-            onClickNegativeButton = {},
-        ),
-        batteryMessageData = MessageCardSlot.Parameters(
-            icon = R.drawable.battery_error,
-            title = "Battery Problem",
-            description = "There is an battery error.",
-            positiveButtonText = "Check battery",
-            negativeButtonText = "Close",
-            onClickPositiveButton = {},
-            onClickNegativeButton = {},
-        ),
-        connectionMessageData = MessageCardSlot.Parameters(
-            icon = R.drawable.wifi_error,
-            title = "Connection Problem",
-            description = "There is an internet connection error.",
-            positiveButtonText = "Report an issue",
-            negativeButtonText = "Cancel",
-            onClickPositiveButton = {},
-            onClickNegativeButton = {},
-        )
+        errorType = homeScreenPreviewData.value().errorType,
+        generalMessageData = homeScreenPreviewData.value().generalMessageData,
+        batteryMessageData = homeScreenPreviewData.value().batteryMessageData,
+        connectionMessageData = homeScreenPreviewData.value().connectionMessageData,
     )
 }
